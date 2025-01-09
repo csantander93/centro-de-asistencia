2c3bdb5dde011b1c5d707d88ec9abc6070c083c1
csantander93 <casdesarrollo.2022@gmail.com>
2025-01-08 22:30:14 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Escuchar en todas las interfaces de red
-    port: 5173,        // O el puerto que estés usando
-  },
-  base: '/centro-de-asistencia/', // Reemplaza con el nombre de tu repositorio
+  base: '/centro-de-asistencias/' // Reemplaza con la ruta de tu repositorio
 })
