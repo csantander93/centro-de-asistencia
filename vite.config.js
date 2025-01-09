1763af091054969a1ce076f910bdf4ee9abac532
csantander93 <casdesarrollo.2022@gmail.com>
2025-01-08 23:09:43 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/centro-de-asistencias/' // Reemplaza con la ruta de tu repositorio
+  base: '/centro-de-asistencias/', // El nombre exacto de tu repositorio
 })
