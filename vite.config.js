b891879aa33254bb837de69d1885cbd03053d2d9
csantander93 <casdesarrollo.2022@gmail.com>
2025-01-09 00:09:52 -0300

    slucionando problema con las rutas

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/centro-de-asistencias/', // El nombre exacto de tu repositorio
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+    }
+  }
 })
