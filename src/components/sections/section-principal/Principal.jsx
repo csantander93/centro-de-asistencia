fe31cd9e4e6536409c08c99b11e8bde651050b37
csantander93 <casdesarrollo.2022@gmail.com>
2025-01-15 02:40:06 -0300

    detalles en principal section

--- src/components/sections/section-principal/Principal.jsx
+++ src/components/sections/section-principal/Principal.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./Principal.css";
 
-// Importa la imagen
-import centroAsistenciasLogo from "../../../assets/images/centro-asistencias.jpg";
+import centroAsistenciasLogo from "../../../assets/images/centro-asistencias.png";
 
 const Principal = () => {
   return (
     <section id="principal" className="principal">
       <div className="content">
         <div className="container text-center">
-          <h1>Bienvenido a <span>Centro de Asistencias</span></h1>
+          <h1>
+            Bienvenido a <span>Centro de Asistencias</span>
+          </h1>
           <p>En donde su bienestar es nuestra prioridad!</p>
         </div>
         <div className="principal-logo">
-          {/* Usando la imagen importada */}
           <img src={centroAsistenciasLogo} alt="Centro de Asistencias" />
         </div>
       </div>
